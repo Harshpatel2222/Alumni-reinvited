cdd610ddf3ff6158d5204bb3924f2a2567a830cb
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-05-29 20:51:44 +0530

    register view

--- alumini_reinvited/web/views.py
+++ alumini_reinvited/web/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+from .models import *
+from .forms import CreateUserForm
+
 
 def home_view(request):
     context={}
     return render(request, 'web/main.html', context)
 
+@login_required(login_url='sign_view')
 def profile_view(request):
     context={}
     return render(request, 'web/profile.html', context)
@@ -28,15 +39,46 @@ def contact_view(request):
     return render(request, 'web/contact.html', context)
 
 def sign_view(request):
-    context={}
-    return render(request, 'web/signin.html', context)
+	if request.user.is_authenticated:
+		return redirect('home_view')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home_view')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'web/signin.html', context)
+
 
 def register_view(request):
-    form = UserCreationForm()
-    context={
-        'form' : form
-    }
-    return render(request, 'web/register.html', context)
+	if request.user.is_authenticated:
+		return redirect('home_view')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('sign_view')
+			
+
+		context = {'form':form}
+		return render(request, 'web/register.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 def team_view(request):
     context={}
