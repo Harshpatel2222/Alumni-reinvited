2702fe3d92e521f4dd719c61f33900a262498c68
Nirav415 <niravkaravadra21809@gmail.com>
2021-05-29 19:00:11 +0530

    Team and Contact page added

--- alumini_reinvited/web/urls.py
+++ alumini_reinvited/web/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('',views.home_view, name='home_view'),  
     path('profile/',views.profile_view, name='profile_view'),
     path('connections/',views.connections_view, name='connections_view'),
-    path('events/',views.events_view, name='events'),
-    
+    path('events/',views.events_view, name='events_view'),
+    path('jobs/',views.jobs_view, name='jobs_view'),
+    path('team/',views.team_view, name='team_view'),
+    path('contact/',views.contact_view, name='contact_view'),
+    path('signin/',views.sign_view, name='sign_view'),
 ]
--- alumini_reinvited/web/views.py
+++ alumini_reinvited/web/views.py
@@ -17,3 +17,19 @@ def connections_view(request):
 def events_view(request):
     context={}
     return render(request, 'web/events.html', context)
+
+def jobs_view(request):
+    context={}
+    return render(request, 'web/jobs.html', context)
+
+def contact_view(request):
+    context={}
+    return render(request, 'web/contact.html', context)
+
+def sign_view(request):
+    context={}
+    return render(request, 'web/sign.html', context)
+
+def team_view(request):
+    context={}
+    return render(request, 'web/team.html', context)
